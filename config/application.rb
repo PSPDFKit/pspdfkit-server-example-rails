d8ef568b4e2ee928c68e684d893fdc6cf40acdce
Nutrient <support@nutrient.io>
2025-07-02 13:42:18 +0200

    Nutrient Web SDK Rails Example

---
+++ config/application.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require_relative "boot"
+
+require "rails"
+
+require "active_model/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_view/railtie"
+require "sprockets/railtie"
+
+# Require the gems listed in Gemfile, including any gems
+# you"ve limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module PSSample
+  class Application < Rails::Application
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+  end
+end
