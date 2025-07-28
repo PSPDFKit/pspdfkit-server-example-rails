a8b69dcb8d02df414635a64da7991bef214d7bf0
Nutrient <support@nutrient.io>
2025-07-28 13:15:15 +0800

    Nutrient Web SDK Rails Example

---
+++ config/boot.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
+
+require "bundler/setup" # Set up gems listed in the Gemfile.
+
+# https://github.com/rails/rails/issues/54260
+require "logger"
